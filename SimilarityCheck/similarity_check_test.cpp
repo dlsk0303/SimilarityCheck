aa979a322de70e374164c977f5c475c9505ca7c7
Yina Yoon <dlsk030303@naver.com>
2025-03-28 10:38:06 +0900

    calculate length

--- SimilarityCheck/similarity_check_test.cpp
+++ SimilarityCheck/similarity_check_test.cpp
@@ -3,6 +3,53 @@
 
 using namespace testing;
 
+class SimilarityCheckerFixture : public Test {
+public:
+	SimilarityChecker app;
+};
+
+TEST_F(SimilarityCheckerFixture, 0퍼센트유사도체크) {
+	int expected = 0;
+	int actual = app.checkSimilarity("", "A");
+	EXPECT_EQ(expected, actual);
+}
+
+TEST_F(SimilarityCheckerFixture, 100퍼센트유사도체크) {
+	int expected = 100;
+	int actual = app.checkSimilarity("ABC", "ABC");
+	EXPECT_EQ(expected, actual);
+}
+
+TEST_F(SimilarityCheckerFixture, 글자수검사하기) {
+	int expected = 60;
+	int actual = app.checkSimilarity("ABC", "DEF");
+	EXPECT_EQ(expected, actual);
+}
+
+TEST_F(SimilarityCheckerFixture, 부분글자수검사하기1) {
+	int expected = 20;
+	int actual = app.checkSimilarity("AAABB", "BAA");
+	EXPECT_EQ(expected, actual);
+}
+
+TEST_F(SimilarityCheckerFixture, 부분글자수검사하기2) {
+	int expected = 30;
+	int actual = app.checkSimilarity("AA", "AAE");
+	EXPECT_EQ(expected, actual);
+}
+
+TEST_F(SimilarityCheckerFixture, 2배글자수검사하기1) {
+	int expected = 0;
+	int actual = app.checkSimilarity("ABCDEF", "GHI");
+	EXPECT_EQ(expected, actual);
+}
+
+TEST_F(SimilarityCheckerFixture, 2배글자수검사하기2) {
+	int expected = 0;
+	int actual = app.checkSimilarity("ABC", "DEFGHI");
+	EXPECT_EQ(expected, actual);
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
